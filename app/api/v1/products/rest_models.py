b34e72fb905540daebf4075967a67742b4ccef5a
pdnlk <facebal900@mail.ru>
2024-11-18 22:09:59 +0300

    change products schemas

--- app/api/v1/products/rest_models.py
+++ app/api/v1/products/rest_models.py
@@ -1,4 +1,12 @@
+from typing import Literal, Optional
+
 from pydantic import BaseModel, Field
+
+
+class ProductParameters(BaseModel):
+    color: Literal['red', 'blue', 'white']
+    style: Literal['sport', 'daily', 'home', 'office']
+    season: Literal['winter', 'summer', 'demi_season']
 
 
 class AddProductRequest(BaseModel):
@@ -7,4 +15,4 @@ class AddProductRequest(BaseModel):
     price: int = Field(gt=0)
     counts: int = Field(gt=0)
     description: str = Field(max_length=1000)
-
+    parameters: Optional[ProductParameters] = None
