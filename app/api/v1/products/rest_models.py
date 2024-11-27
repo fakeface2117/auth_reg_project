f8bfddbc2d9aa492cdc2f61bb29b4e98b09a816a
pdnlk <facebal900@mail.ru>
2024-11-27 20:56:09 +0300

    feat: union data about sizes by pydantic model

--- app/api/v1/products/rest_models.py
+++ app/api/v1/products/rest_models.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from datetime import datetime
 from typing import Literal, Optional, List, Any
 
-from pydantic import BaseModel, Field, ConfigDict, model_validator
+from pydantic import BaseModel, Field, ConfigDict, model_validator, field_validator
 
 
 class BaseMappedModel(BaseModel):
@@ -23,9 +24,6 @@ class SizesCountsModel(BaseModel):
     size: Literal['XS', 'S', 'M', 'L', 'XL', 'XXL']
     count: int = Field(ge=0)
 
-    def __hash__(self):
-        return hash(self.size)
-
 
 class AddProductRequest(BaseModel):
     name: str = Field(max_length=50)
@@ -39,6 +37,7 @@ class AddProductRequest(BaseModel):
     @model_validator(mode='before')
     @classmethod
     def validate_sums(cls, data: Any):
+        """Проверка соответствия общего количества товара и количества по размерам товаров"""
         if isinstance(data, dict):
             sum_count = data.get('sum_count')
             counts = data.get('counts')
@@ -46,6 +45,15 @@ class AddProductRequest(BaseModel):
             if sum_count != sizes_data:
                 raise ValueError(f"Несоответствие данных по размерам ({sizes_data}) и общему количеству ({sum_count})")
             return data
+
+    @field_validator('counts')
+    def correct_counts(cls, value: List[SizesCountsModel] | None):
+        """Функция позволяет на этапе валидации объединить данные по нескольким размерам, если они повторяются"""
+        if value:
+            grouped_dict = defaultdict(int)
+            for item in value:
+                grouped_dict[item.size] += item.count
+            return [SizesCountsModel(size=s, count=c) for s, c in grouped_dict.items()]
 
 
 class GetAllProductInfo(BaseMappedModel):
@@ -83,7 +91,8 @@ class ProductsFilters(BaseModel):
     price_min: int = Field(gt=0, default=1)
     price_max: int = Field(le=1000000000, default=1000000000)
 
-class UpdatedProductData(BaseModel): # TODO доделать валидацию для обновления и сделать метод чисто для админа и протестить
+
+class UpdatedProductData(BaseModel):
     name: str | None = Field(max_length=50, default=None)
     brand: str | None = Field(max_length=50, default=None)
     price: int | None = Field(gt=0, le=1000000000, default=None)
@@ -108,5 +117,15 @@ class UpdatedProductData(BaseModel): # TODO доделать валидацию 
                     sum_count = sizes_data
                     data['sum_count'] = sum_count
                 if sum_count != sizes_data:
-                    raise ValueError(f"Несоответствие данных по размерам ({sizes_data}) и общему количеству ({sum_count})")
+                    raise ValueError(
+                        f"Несоответствие данных по размерам ({sizes_data}) и общему количеству ({sum_count})")
             return data
+
+    @field_validator('counts')
+    def correct_counts(cls, value: List[SizesCountsModel] | None):
+        """Функция позволяет на этапе валидации объединить данные по нескольким размерам, если они повторяются"""
+        if value:
+            grouped_dict = defaultdict(int)
+            for item in value:
+                grouped_dict[item.size] += item.count
+            return [SizesCountsModel(size=s, count=c) for s, c in grouped_dict.items()]
