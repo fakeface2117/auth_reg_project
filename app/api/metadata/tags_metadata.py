5bc31bc8199d5ca02d2afccca82244b384ceae07
pdnlk <facebal900@mail.ru>
2025-01-13 01:15:02 +0300

    fix: tags_metadata.py

--- app/api/metadata/tags_metadata.py
+++ app/api/metadata/tags_metadata.py
@@ -18,5 +18,8 @@ tags_metadata = [
     },{
         "name": "Orders",
         "description": "Группа роутеров по работе с заказами"
+    },{
+        "name": "Payment",
+        "description": "Платежные операции"
     }
 ]
